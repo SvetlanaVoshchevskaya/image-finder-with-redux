10a4b9a31a4fc00980ec3528c14156a8aa093666
Svetlana <VSveta.83@gmail.com>
2019-10-19 21:47:36 +0300

    Updates

---
+++ precache-manifest.e0b2a233492d0bc92a2adb63787f9f2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5245251a9bbac1b2118883a6f6e3bd09",
+    "url": "/image-finder-with-redux/index.html"
+  },
+  {
+    "revision": "b1270f133275583fa36a",
+    "url": "/image-finder-with-redux/static/css/main.139c8694.chunk.css"
+  },
+  {
+    "revision": "f73f848c896c8cb94410",
+    "url": "/image-finder-with-redux/static/js/2.771664c4.chunk.js"
+  },
+  {
+    "revision": "b1270f133275583fa36a",
+    "url": "/image-finder-with-redux/static/js/main.fac54e1d.chunk.js"
+  },
+  {
+    "revision": "c9b5e87f0e9897c7d02d",
+    "url": "/image-finder-with-redux/static/js/runtime~main.d2f1cd52.js"
+  }
+]);
